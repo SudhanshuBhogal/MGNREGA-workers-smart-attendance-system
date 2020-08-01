71f3ca65c5c02315cfee9020d6a93b26eed81d65
Sudhanshu Bhogal <sudhanshubhogal30299@gmail.com>
2020-08-01 11:01:31 +0530

    Added android files

---
+++ Android/app/src/main/java/com/example/stech20/Custom_dialog.java
@@ -0,0 +1,14 @@
+package com.example.stech20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Custom_dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_dialog);
+    }
+}
---
+++ Android/app/src/main/java/com/example/stech20/Type.java
@@ -0,0 +1,102 @@
+package com.example.stech20;
+
+public class Type {
+
+    private String contact;
+    private String Email;
+    private String Name;
+    private String password;
+    private String type;
+    private String address;
+    private double latitude;
+    private double longitude;
+
+    public Type()
+    {
+
+    }
+
+    public Type(String name, String contact, String email, String password, String type) {
+        this.contact = contact;
+        this.Email = email;
+        this.Name = name;
+        this.password=password;
+        this.type = type;
+    }
+
+    public Type(String name, String contact, String email, String password, String type,String address, double latitude, double longitude) {
+        this.contact = contact;
+        this.Email = email;
+        this.Name = name;
+        this.password=password;
+        this.type = type;
+        this.address=address;
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setAddress(String pass) {
+        this.password = pass;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+}
