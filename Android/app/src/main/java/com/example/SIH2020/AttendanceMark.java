264ca46e107323eb412c40066c5c594a529b483d
yash <yashsharangpani@gmail.com>
2020-08-01 23:36:35 +0530

    Utility classes added for REST API

---
+++ Android/app/src/main/java/com/example/SIH2020/AttendanceMark.java
@@ -0,0 +1,12 @@
+package com.example.SIH2020;
+
+public class AttendanceMark {
+
+    String name;
+
+    public AttendanceMark(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+}
